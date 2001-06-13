1bc2a69c1b9342859c8dd8621156d86fb52b1732
Roscoe A. Bartlett <rabartl@sandia.gov>
2001-06-13 16:26:17 +0000

    Added initalization for QPSchur for relaxed Hessian.

---
+++ src/ConstrainedOptPack/src/qpsolvers/ConstrainedOptPack_QPSchurInitKKTSystemHessianRelaxed.cpp
@@ -0,0 +1,101 @@
+// /////////////////////////////////////////////////////////
+// QPSchurInitKKTSystemHessianRelaxed.cpp
+
+#include "ConstrainedOptimizationPack/include/QPSchurInitKKTSystemHessianRelaxed.h"
+#include "ConstrainedOptimizationPack/include/MatrixSymHessianRelaxNonSing.h"
+#include "SparseLinAlgPack/include/GenPermMatrixSlice.h"
+#include "SparseLinAlgPack/include/GenPermMatrixSliceOp.h"
+#include "SparseLinAlgPack/include/SpVectorOp.h"
+#include "LinAlgPack/include/LinAlgOpPack.h"
+#include "Misc/include/dynamic_cast_verbose.h"
+
+namespace LinAlgOpPack {
+	using SparseLinAlgPack::Vp_StV;
+	using SparseLinAlgPack::Vp_StMtV;
+}
+
+namespace ConstrainedOptimizationPack {
+
+void QPSchurInitKKTSystemHessianRelaxed::initialize_kkt_system(
+	const VectorSlice&    g
+	,const MatrixWithOp&  G
+	,value_type           etaL
+	,const SpVectorSlice& dL
+	,const SpVectorSlice& dU
+	,const MatrixWithOp*  F
+	,BLAS_Cpp::Transp     trans_F
+	,const VectorSlice*   f
+	,size_type*           n_R
+	,i_x_free_t*          i_x_free
+	,i_x_fixed_t*         i_x_fixed
+	,bnd_fixed_t*         bnd_fixed
+	,j_f_decomp_t*        j_f_decomp
+	,Vector*              b_X
+	,Ko_ptr_t*            Ko
+	,Vector*              fo
+	) const
+{
+	using BLAS_Cpp::trans;
+
+	// Validate type of and convert G
+	const MatrixSymHessianRelaxNonSing
+		*G_relax_ptr = dynamic_cast<const MatrixSymHessianRelaxNonSing*>(&G);
+
+	if( G_relax_ptr == NULL ) {
+		init_kkt_full_.initialize_kkt_system(
+			g,G,etaL,dL,dU,F,trans_F,f,n_R,i_x_free,i_x_fixed,bnd_fixed
+			,j_f_decomp,b_X,Ko,fo);
+		return;
+	}
+
+	const MatrixSymHessianRelaxNonSing
+		&G_relax = *G_relax_ptr;
+
+	// get some stuff
+	const MatrixSymWithOpFactorized
+		&G_orig = G_relax.G(),
+		&M      = G_relax.M();
+	const size_type
+		nd = g.size(),
+		no = G_orig.rows(),
+		nr = M.rows();
+	assert( no + nr == nd );
+
+	// Setup output arguments
+
+	// n_R = nd_R
+	*n_R = no;
+	// i_x_free.size() == 0 and i_x_free is implicitly identity
+	i_x_free->resize(0);
+	// i_x_fixed[]
+	i_x_fixed->resize(nr+1);
+	if(nr) {
+		// i_x_fixed[l-1] = no + l, l = 1...nr
+		for( size_type l = 1; l <= nr; ++l )
+			(*i_x_fixed)[l-1] = no+nr+1;
+	}
+	(*i_x_fixed)[nr] = nd+1; // extra relaxation is always initially active
+	// bnd_fixed[]
+	bnd_fixed->resize(nr+1);
+	if(nr) {
+		// bnd_fixed[l-1] = LOWER, l = 1...nr
+		std::fill_n( bnd_fixed->begin(), nr, LOWER );
+	}
+	(*bnd_fixed)[nr] = LOWER; // relaxation is always initially active
+	// j_f_decomp[]
+	j_f_decomp->resize(0);
+	// b_X
+	b_X->resize(nr+1);
+	if(nr) {
+		// b_X[l-1] = dL(no+l), l = 1...nr
+		LinAlgOpPack::assign( &(*b_X)(1,nr), dL(no+1,no+nr) );
+	}
+	(*b_X)[nr] = etaL; // relaxation is always initially active
+	// Ko = G.G
+	*Ko = G_relax.G_ptr(); // now B_RR is a shared object
+	// fo = - *g(1:no)
+	LinAlgOpPack::V_StV( fo, -1.0, g(1,no) );
+
+}
+
+} // end namesapce ConstrainedOptimizationPack
---
+++ src/ConstrainedOptPack/src/qpsolvers/ConstrainedOptPack_QPSchurInitKKTSystemHessianRelaxed.hpp
@@ -0,0 +1,84 @@
+// ///////////////////////////////////////////////////////////////////
+// QPSchurInitKKTSystemHessianRelaxed.h
+
+#ifndef QPSCHUR_INIT_KKT_SYSTEM_HESSIAN_RELAXED_H
+#define QPSCHUR_INIT_KKT_SYSTEM_HESSIAN_RELAXED_H
+
+#include "QPSolverRelaxedQPSchur.h"
+#include "QPSchurInitKKTSystemHessianFull.h"
+
+namespace ConstrainedOptimizationPack {
+
+///
+/** Implementation of initial KKT system where all original variables
+ * are free and all the relaxation variables are fixed.
+ *
+ * In this implementation, #G# should support the \Ref{MatrixSymHessianRelaxNonSing}
+ * interface.   Otherwise, it will try the \Ref{MatrixSymWithOpFactorized} interface
+ * using the base implementation of \Ref{QPSchurInitKKTSystemHessianFull}.
+ */
+class QPSchurInitKKTSystemHessianRelaxed
+	: public QPSchurInitKKTSystemHessianFull
+{
+public:
+
+	// ////////////////////////////////
+	// Overridden from InitKKTSystem
+
+	///
+	/** Initialize the KKT system where the original variables are initiallly 
+	 * free and all the relaxation variables are fixed and their are no
+	 * constraints in Ko.
+	 *
+	 * The Hessian for the QP without the relaxation #G# is represented as
+	 * a \Ref{MatrixSymHessianRelaxNonSing} object and is:
+     \begin{verbatim}
+	 G = [ G_orig     ]
+	     [          M ]
+	 \end{verbatim}
+	 * If #G# does not support the interface #MatrixSymHessianRelaxNonSing# then
+	 * the function #QPSchurInitKKTSystemHessianFull::initialize_kkt_system(...)#
+	 * will be called.
+	 *
+	 * Given the above parts of #G#, define: #[no,no] = size(G.G)# and
+	 * #[nr,nr] = size(G.M)#.  Then initial KKT system is defined as:
+	 *
+	 * #n_R = no#\\
+	 * #i_x_free.size() == 0# and #i_x_free is implicitly identity#\\
+	 * #i_x_fixed[l-1] = no + l, l = 1...nr#\\
+	 * #i_x_fixed[nr] = no+nr+1#\\
+	 * #bnd_fixed[l-1] = LOWER, l = 1...nr#\\
+	 * #bnd_fixed[nr] = LOWER#\\
+	 * #j_f_decomp[] = empty#\\
+	 * #b_X[l-1] = dL(no+l), l = 1...nr#\\
+	 * #b_X[nr] = etaL#\\
+	 * #Ko = G.G#\\
+	 * #fo = - g(1:no)#\\\
+	 */
+	void initialize_kkt_system(
+		const VectorSlice&    g
+		,const MatrixWithOp&  G
+		,value_type           etaL
+		,const SpVectorSlice& dL
+		,const SpVectorSlice& dU
+		,const MatrixWithOp*  F
+		,BLAS_Cpp::Transp     trans_F
+		,const VectorSlice*   f
+		,size_type*           n_R
+		,i_x_free_t*          i_x_free
+		,i_x_fixed_t*         i_x_fixed
+		,bnd_fixed_t*         bnd_fixed
+		,j_f_decomp_t*        j_f_decomp
+		,Vector*              b_X
+		,Ko_ptr_t*            Ko
+		,Vector*              fo
+		) const;
+
+private:
+	QPSchurInitKKTSystemHessianFull  init_kkt_full_;
+
+}; // end class QPSchurInitKKTSystemHessianRelaxed
+
+} // end namesapce ConstrainedOptimizationPack
+
+#endif // QPSCHUR_INIT_KKT_SYSTEM_HESSIAN_RELAXED_H
