00325bd0fb693122fc0ecd67a6d348358a75b89e
Roscoe A. Bartlett <rabartl@sandia.gov>
2001-11-13 22:31:57 +0000

    Added a source file for previously inlined method

---
+++ src/IterationPack/src/IterationPack_IterQuantity.cpp
@@ -0,0 +1,26 @@
+// /////////////////////////////////////////////////////
+// IterQuantity.cpp
+
+#include "GeneralIterationPack/include/IterQuantity.h"
+#include "ThrowException.h"
+
+namespace GeneralIterationPack {
+
+void IterQuantity::assert_has_storage_k(int offset) const {
+	if(!has_storage_k(offset))
+		THROW_EXCEPTION(
+			true, NoStorageAvailable
+			,"IterQuantity::assert_has_storage_k(offset) : There is not storage available for "
+				<< name() << " for the k"	<< std::showpos << offset << " iteration" );
+}
+
+void IterQuantity::assert_updated_k(int offset) const {
+	if(!updated_k(offset))
+		THROW_EXCEPTION(
+			true, QuanityNotSet
+			,"IterQuantity::assert_updated_k(offset) : The interation quantity " << name() 
+			<< " has not been been updated for the k"	<< std::showpos << offset
+			<< " iteration yet." );
+}
+
+} // end namespace GeneralIterationPack
