1c03a8a309de9143b5cea741fbdad8f8d6688e57
Roscoe A. Bartlett <rabartl@sandia.gov>
2001-12-17 18:28:35 +0000

    Added testing function for BasisSystem with the NLP object.

---
+++ src/NLPInterfacePack/src/abstract/test/NLPInterfacePack_test_basis_system.cpp
@@ -0,0 +1,89 @@
+// /////////////////////////////////////////////////////////////////////
+// test_basis_system.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include "test_basis_system.h"
+#include "NLPInterfacePack/include/NLPFirstOrderInfo.h"
+#include "AbstractLinAlgPack/include/BasisSystem.h"
+#include "AbstractLinAlgPack/include/BasisSystemTester.h"
+#include "AbstractLinAlgPack/include/BasisSystemTesterSetOptions.h"
+#include "AbstractLinAlgPack/include/MatrixSpace.h"
+#include "AbstractLinAlgPack/include/MatrixWithOpNonsingular.h"
+
+bool NLPInterfacePack::test_basis_system(
+ 	NLPFirstOrderInfo*                             nlp
+	,BasisSystem*                                  basis_sys
+	,OptionsFromStreamPack::OptionsFromStream*     options
+	,std::ostream*                                 out
+	)
+{
+	namespace rcp = ReferenceCountingPack;
+
+	const index_type
+		n = nlp->n(),
+		m = nlp->m(),
+		mI = nlp->mI();
+
+	// Create the matrices Gc and Gh
+	NLPFirstOrderInfo::mat_space_ptr_t::element_type::mat_ptr_t
+		Gc = ( m  ? nlp->space_Gc()->create_member() : NULL ),
+		Gh = ( mI ? nlp->space_Gh()->create_member() : NULL );
+	
+	// Compute the matrices at xinit
+	const VectorWithOp
+		&xo = nlp->xinit();
+	if(m)
+		nlp->set_Gc(Gc.get());
+	if(mI)
+		nlp->set_Gh(Gh.get());
+	if(m)
+		nlp->calc_Gc(xo);
+	if(mI)
+		nlp->calc_Gh(xo,m==0);
+
+	// Create the matrices C and D
+	BasisSystem::space_C_ptr_t::element_type::mat_ptr_t
+		C = ( m ? basis_sys->space_C()->create_member() : NULL);
+	BasisSystem::space_D_ptr_t::element_type::mat_ptr_t
+		D = ( m && n > m ? basis_sys->space_C()->create_member() : NULL);
+
+	// Initialize C and D with basis_sys
+	basis_sys->update_basis(
+		Gc.get()
+		,Gh.get()
+		,C.get()
+		,D.get()
+		);
+
+	// Test the basis and basis system objects.
+	BasisSystemTester
+		basis_sys_tester;
+	if(options) {
+		BasisSystemTesterSetOptions
+			opt_setter(&basis_sys_tester);
+		opt_setter.set_options(*options);
+	}
+	const bool print_all_warnings = false; // ToDo: set somehow?
+	const bool result = basis_sys_tester.test_basis_system(
+		*basis_sys
+		,Gc.get()
+		,Gh.get()
+		,C.get()
+		,D.get()
+		,print_all_warnings
+		,out
+		);
+
+	return result;
+}
---
+++ src/NLPInterfacePack/src/abstract/test/NLPInterfacePack_test_basis_system.hpp
@@ -0,0 +1,41 @@
+// //////////////////////////////////////////////////////////////
+// test_basis_system.h
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#ifndef TEST_BASIS_SYSTEM_H
+#define TEST_BASIS_SYSTEM_H
+
+#include "NLPInterfacePack/include/NLPInterfacePackTypes.h"
+
+namespace OptionsFromStreamPack {
+	class OptionsFromStream;
+}
+
+namespace NLPInterfacePack {
+
+///
+/** Test a \c BasisSystem object given matrices from a compatible \c NLPFirstOrderInfo object.
+ *
+ * ToDo: Finish documentation!
+ */
+bool test_basis_system(
+ 	NLPFirstOrderInfo*                             nlp
+	,BasisSystem*                                  basis_sys
+	,OptionsFromStreamPack::OptionsFromStream*     options
+	,std::ostream*                                 out
+	);
+
+} // end NLPInterfacePack
+
+#endif //TEST_BASIS_SYSTEM_H
