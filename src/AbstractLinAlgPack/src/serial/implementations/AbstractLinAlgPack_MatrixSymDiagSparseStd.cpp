43944a996c812c14dc106c86eda546cb6c64c9cb
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-11 01:04:43 +0000

    Changed name form MatrixDiagonalSparseSubclass to MatrixSymDiagonalSparseStd and updated for new design

---
+++ src/AbstractLinAlgPack/src/serial/implementations/AbstractLinAlgPack_MatrixSymDiagSparseStd.cpp
@@ -0,0 +1,62 @@
+// //////////////////////////////////////////////////////////////////////////////////
+// MatrixSymDiagonalSparseStd.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include "SparseLinAlgPack/include/MatrixSymDiagonalSparseStd.h"
+#include "SparseLinAlgPack/include/SpVectorOut.h"
+#include "LinAlgPack/include/VectorClass.h"
+#include "LinAlgPack/include/LinAlgPackAssertOp.h"
+#include "ThrowException.h"
+
+namespace SparseLinAlgPack {
+
+MatrixSymDiagonalSparseStd::MatrixSymDiagonalSparseStd( const SpVectorSlice& diag )
+	: diag_(diag)
+{}
+
+void MatrixSymDiagonalSparseStd::initialize( const SpVectorSlice& diag )
+{
+	diag_ = diag;
+}
+
+// Overridden from MatrixWithOp
+
+MatrixWithOp& MatrixSymDiagonalSparseStd::operator=(const MatrixWithOp& m)
+{
+	if(&m == this) return *this;	// assignment to self
+	const MatrixSymDiagonalSparseStd
+		*p_m = dynamic_cast<const MatrixSymDiagonalSparseStd*>(&m);
+	if(p_m) {
+		diag_ = p_m->diag_;
+	}
+	else {
+		THROW_EXCEPTION(
+			true, std::invalid_argument
+			,"MatrixSymDiagonalSparseStd::operator=(const MatrixWithOp& m) : Error!"
+			"The concrete type of m = \'" << typeid(m).name() << "\' is not a subclass of "
+			"MatrixSymDiagonalSparseStd as expected"
+			);
+	}
+	return *this;
+}
+
+// Overridden from MatrixDiagonalSparse
+
+const SpVectorSlice MatrixSymDiagonalSparseStd::diag() const
+{
+	return diag_();
+}
+
+
+}	// end namespace SparseLinAlgPack
---
+++ src/AbstractLinAlgPack/src/serial/implementations/AbstractLinAlgPack_MatrixSymDiagSparseStd.hpp
@@ -0,0 +1,78 @@
+// ///////////////////////////////////////////////////////////////////
+// MatrixSymDiagonalSparseStd.h
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#ifndef SPARSE_LINALG_PACK_MATRIX_DIAGONAL_SPARSE_STD_H
+#define SPARSE_LINALG_PACK_MATRIX_DIAGONAL_SPARSE_STD_H
+
+#include "MatrixSymDiagonalSparse.h"
+#include "AbstractLinAlgPack/include/SpVectorClass.h"
+
+namespace SparseLinAlgPack {
+
+///
+/** Concrete subclass for a serial symmetric diagonal matrix with many zeros on the diagonal.
+  *
+  * The underlying diagonal vector is sorted and determines the dimensions of the
+  * matrix.
+  *
+  * The default constructor, copy constructor are allowed.
+  */
+class MatrixSymDiagonalSparseStd: virtual public MatrixSymDiagonalSparse {
+public:
+
+	/** @name Constructors/initializes */
+	//@{
+
+	/// Construct uninitialized
+	MatrixSymDiagonalSparseStd()
+	{}
+
+	///
+	/** Construct the diagonal.
+	  */
+	MatrixSymDiagonalSparseStd( const SpVectorSlice& diag );
+
+	///
+	/** Reinitialize the diagonal.
+	  */
+	void initialize( const SpVectorSlice& diag );
+
+	//@}
+
+	/** @name Overridden from MatrixWithOp */
+	//@{
+
+	///
+	MatrixWithOp& operator=(const MatrixWithOp& m);
+
+	//@}
+
+	/** Overridden from MatrixDiagonalSparse */
+	//@{
+
+	///
+	const SpVectorSlice diag() const;
+
+	//@}
+
+private:
+	
+	SpVector	diag_;
+
+};	// end class MatrixDiagonalSparse
+
+}	// end namespace SparseLinAlgPack
+
+#endif	// SPARSE_LINALG_PACK_MATRIX_DIAGONAL_SPARSE_STD_H
