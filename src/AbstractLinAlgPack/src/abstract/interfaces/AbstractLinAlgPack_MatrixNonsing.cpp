2ec4dd1cfcedcc00ad38826624074c3ccd010272
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-11 00:47:49 +0000

    Cleanup up formating

--- src/AbstractLinAlgPack/src/abstract/interfaces/AbstractLinAlgPack_MatrixNonsing.cpp
+++ src/AbstractLinAlgPack/src/abstract/interfaces/AbstractLinAlgPack_MatrixNonsing.cpp
@@ -26,22 +26,22 @@ namespace AbstractLinAlgPack {
 // Level-2 BLAS
 
 void MatrixNonsingular::V_InvMtV(
-	VectorWithOpMutable* v_lhs, BLAS_Cpp::Transp trans_rhs1
-	, const SpVectorSlice& sv_rhs2) const
+	VectorWithOpMutable* v_lhs, BLAS_Cpp::Transp trans_rhs1, const SpVectorSlice& sv_rhs2
+	) const
 {
 	assert(0); // ToDo: Implement!
 }
 
 value_type MatrixNonsingular::transVtInvMtV(
-	const VectorWithOp& v_rhs1
-	, BLAS_Cpp::Transp trans_rhs2, const VectorWithOp& v_rhs3) const
+	const VectorWithOp& v_rhs1, BLAS_Cpp::Transp trans_rhs2, const VectorWithOp& v_rhs3
+	) const
 {
 	assert(0); // ToDo: Implement!
 }
 
 value_type MatrixNonsingular::transVtInvMtV(
-	const SpVectorSlice& sv_rhs1
-	, BLAS_Cpp::Transp trans_rhs2, const SpVectorSlice& sv_rhs3) const
+	const SpVectorSlice& sv_rhs1, BLAS_Cpp::Transp trans_rhs2, const SpVectorSlice& sv_rhs3
+	) const
 {
 	assert(0); // ToDo: Implement!
 }
@@ -50,16 +50,18 @@ value_type MatrixNonsingular::transVtInvMtV(
 
 void MatrixNonsingular::M_StInvMtM(
 	MatrixWithOp* m_lhs, value_type alpha
-	,BLAS_Cpp::Transp trans_rhs1, const MatrixWithOp& mwo_rhs2
-	,BLAS_Cpp::Transp trans_rhs2) const
+	,BLAS_Cpp::Transp trans_rhs1
+	,const MatrixWithOp& mwo_rhs2, BLAS_Cpp::Transp trans_rhs2
+	) const
 {
 	assert(0); // ToDo: Implement!
 }
 
-void MatrixNonsingular::M_StInvMtM(
+void MatrixNonsingular::M_StMtInvM(
 	MatrixWithOp* g_lhs, value_type alpha
-	, const MatrixWithOp& mwo_rhs1, BLAS_Cpp::Transp trans_rhs1
-	, BLAS_Cpp::Transp trans_rhs2) const
+	,const MatrixWithOp& mwo_rhs1, BLAS_Cpp::Transp trans_rhs1
+	,BLAS_Cpp::Transp trans_rhs2
+	) const
 {
 	assert(0); // ToDo: Implement!
 }
