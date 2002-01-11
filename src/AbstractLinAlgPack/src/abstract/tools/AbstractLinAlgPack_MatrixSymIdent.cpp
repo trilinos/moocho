fe335e98659e97e110cbc31d0fcbd5d0d3db9b2a
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-11 00:45:04 +0000

    Added MatrixSymIdentity subclass for abstract linear algebra

---
+++ src/AbstractLinAlgPack/src/abstract/tools/AbstractLinAlgPack_MatrixSymIdent.cpp
@@ -0,0 +1,93 @@
+// ///////////////////////////////////////////////////////////
+// MatrixSymIdentity.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include <assert.h>
+
+#include <ostream>
+
+#include "AbstractLinAlgPack/include/MatrixSymIdentity.h"
+#include "AbstractLinAlgPack/include/VectorStdOps.h"
+#include "AbstractLinAlgPack/include/LinAlgOpPack.h"
+
+namespace AbstractLinAlgPack {
+
+// Constructors/initalizers
+
+MatrixSymIdentity::MatrixSymIdentity(
+	const VectorSpace::space_ptr_t&          vec_space
+	,const value_type                        scale
+	)
+{
+	this->initialize(vec_space,scale);
+}
+
+void MatrixSymIdentity::initialize(
+	const VectorSpace::space_ptr_t&          vec_space
+	,const value_type                        scale
+	)
+{
+	vec_space_ = vec_space;
+	scale_     = scale;
+}
+
+// Overridden from MatrixBase
+
+size_type MatrixSymIdentity::rows() const
+{
+	return vec_space_.get() ? vec_space_->dim() : 0;
+}
+
+size_type MatrixSymIdentity::nz() const
+{
+	return vec_space_.get() ? vec_space_->dim() : 0;
+}
+
+// Overridden from MatrixWithOp
+
+const VectorSpace& MatrixSymIdentity::space_cols() const {
+	return *vec_space_;
+}
+
+const VectorSpace& MatrixSymIdentity::space_rows() const {
+	return *vec_space_;
+}
+
+std::ostream& MatrixSymIdentity::output(std::ostream& out) const
+{
+	out << "Identity matrix of size " << rows() << " x " << rows() << std::endl;
+	return out;
+}
+
+void MatrixSymIdentity::Vp_StMtV(
+	VectorWithOpMutable* y, value_type a, BLAS_Cpp::Transp M_trans
+	,const VectorWithOp& x, value_type b
+	) const
+{
+	AbstractLinAlgPack::Vp_MtV_assert_sizes( y->dim(), rows(), cols(), BLAS_Cpp::no_trans, x.dim() );
+	Vt_S(y,b);
+    Vp_StV(y,a*scale_,x);
+}
+
+// Overridden from MatrixNonsingular
+
+void MatrixSymIdentity::V_InvMtV(
+	VectorWithOpMutable* y, BLAS_Cpp::Transp M_trans, const VectorWithOp& x
+	) const
+{
+	AbstractLinAlgPack::Vp_MtV_assert_sizes( y->dim(), rows(), cols(), BLAS_Cpp::no_trans, x.dim() );
+	LinAlgOpPack::V_StV(y,1.0/scale_,x);
+}
+
+} // end namespace AbstractLinAlgPack
---
+++ src/AbstractLinAlgPack/src/abstract/tools/AbstractLinAlgPack_MatrixSymIdent.hpp
@@ -0,0 +1,111 @@
+// ///////////////////////////////////////////////////////////////////////
+// MatrixSymIdentity.h
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#ifndef ALAP_MATRIX_SYM_IDENTITY_H
+#define ALAP_MATRIX_SYM_IDENTITY_H
+
+#include "MatrixSymWithOpNonsingular.h"
+#include "VectorSpace.h"
+
+namespace AbstractLinAlgPack {
+
+///
+/** Matrix subclass for a scaled identity matrix.
+ *
+ * More operations will be overridden as they are needed by various applications.
+ */
+class MatrixSymIdentity : virtual public MatrixSymWithOpNonsingular {
+public:
+	
+	/** @name Constructors/initializers */
+	//@{
+
+	/// Calls <tt>this->initialize()</tt>.
+	MatrixSymIdentity(
+		const VectorSpace::space_ptr_t&          vec_space = NULL
+		,const value_type                        scale     = 1.0
+		);
+
+	///
+	void initialize(
+		const VectorSpace::space_ptr_t&          vec_space
+		,const value_type                        scale
+	);
+
+	//@}
+
+	/** @name Access */
+	//@{
+
+	///
+	value_type scale() const;
+
+	//@}
+
+	/** @name Overridden from MatrixBase */
+	//@{
+
+	/// Returns 0 if not initalized.
+	size_type rows() const;
+	/// Returns <tt>this->rows()</tt>
+	size_type nz() const;
+
+	//@}
+
+	/** @name Overridden from MatrixWithOp */
+	//@{
+
+	///
+	const VectorSpace& space_rows() const;
+	///
+	const VectorSpace& space_cols() const;
+	///
+	std::ostream& output(std::ostream& out) const;
+	///
+	void Vp_StMtV(
+		VectorWithOpMutable* v_lhs, value_type alpha, BLAS_Cpp::Transp trans_rhs1
+		,const VectorWithOp& v_rhs2, value_type beta ) const;
+
+	//@}
+
+	/** @name Overridden from MatrixNonsingular */
+	//@{
+
+	///
+	void V_InvMtV(
+		VectorWithOpMutable* v_lhs, BLAS_Cpp::Transp trans_rhs1
+		,const VectorWithOp& v_rhs2 ) const;
+
+	//@}
+
+private:
+
+	VectorSpace::space_ptr_t  vec_space_;
+	value_type                scale_;
+	
+}; // end class MatrixSymIdentity
+
+// ///////////////////////////////////////////
+// Inline members
+
+inline
+value_type MatrixSymIdentity::scale() const
+{
+	return scale_;
+}
+
+} // end namespace AbstractLinAlgPack
+
+#endif // ALAP_MATRIX_SYM_IDENTITY_H
