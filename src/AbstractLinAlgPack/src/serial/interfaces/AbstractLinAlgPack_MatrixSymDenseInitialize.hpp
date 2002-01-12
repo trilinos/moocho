6eb520e4daee01e62a1ef41815e610d98c623e57
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-12 00:00:35 +0000

    Updated for new design

--- src/AbstractLinAlgPack/src/serial/interfaces/AbstractLinAlgPack_MatrixSymDenseInitialize.hpp
+++ src/AbstractLinAlgPack/src/serial/interfaces/AbstractLinAlgPack_MatrixSymDenseInitialize.hpp
@@ -16,21 +16,24 @@
 #ifndef MATRIX_SYM_DENSE_INITIALIZE_H
 #define MATRIX_SYM_DENSE_INITIALIZE_H
 
-#include "Matrix.h"
+#include "AbstractLinAlgPack/include/MatrixBase.h"
 
 namespace SparseLinAlgPack {
 
 ///
-/** Interface for initializing a matrix with a dense symmetric matrix.
+/** Mix-in Interface for initializing a matrix with a dense symmetric matrix.
   */
-class MatrixSymDenseInitialize : public virtual Matrix {
+class MatrixSymDenseInitialize
+	: virtual public AbstractLinAlgPack::MatrixBase // doxygen needs the full name
+{
 public:
 
 	///
 	/** Initialize with a symmetric dense matrix.
-	 * Through this interface there are absolutly no preconditions
-	 * as the the state of #this# after this function executes.
-	 * The implementation can use #M# to initialize itself any way
+	 *
+	 * Through this interface there are absolutly no postconditions
+	 * as the the state of \c this after this function executes.
+	 * The implementation can use \c M to initialize itself any way
 	 * it would like.
 	 */
 	virtual void initialize( const sym_gms& M ) = 0;
