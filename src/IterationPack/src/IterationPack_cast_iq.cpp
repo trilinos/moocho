fa9de5aa906680d02e90b30b2b6ef22be450274e
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-21 23:57:19 +0000

    Added cast_iq<>(...) functions for iq_id.  Improved the error reporting.  Added soure file for throwing exceptions for easier debugging with gcc

---
+++ src/IterationPack/src/IterationPack_cast_iq.cpp
@@ -0,0 +1,45 @@
+// ///////////////////////////////////////////////////////////
+// cast_iq.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include "GeneralIterationPack/include/cast_iq.h"
+#include "ThrowException.h"
+
+void GeneralIterationPack::imp_cast_iq_throw_error(
+	const std::string&                 iq_name
+	,const std::string&                iq_is_type_name
+	,const std::string&                iq_want_type_name
+	)
+{
+	THROW_EXCEPTION(
+		true, GeneralIterationPack::InvalidTypeCastException
+		,"cast_id<T>(state,iq_name) : Error, the iteration quantity \'"
+		<< iq_name << "\' exists with type \'" << iq_is_type_name << "\' but does not "
+		<< "support the \'IterQuantityAccess<" << iq_want_type_name << ">\' interface" );
+}
+
+void GeneralIterationPack::imp_cast_iq_throw_error(
+	const AlgorithmState::iq_id_type   iq_id
+	,const std::string&                iq_name
+	,const std::string&                iq_is_type_name
+	,const std::string&                iq_want_type_name
+	)
+{
+	THROW_EXCEPTION(
+		true, GeneralIterationPack::InvalidTypeCastException
+		,"cast_id<T>(state,iq_id,iq_name) : Error, the iteration quantity \'"
+		<< iq_name << "\' with iq_id = \'" << iq_id
+		<< "\' exists with type \'" << iq_is_type_name << "\' but does not "
+		<< "support the \'IterQuantityAccess<" << iq_want_type_name << ">\' interface" );
+}
--- src/IterationPack/src/IterationPack_cast_iq.hpp
+++ src/IterationPack/src/IterationPack_cast_iq.hpp
@@ -13,10 +13,9 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // above mentioned "Artistic License" for more details.
 
-#ifndef CAST_IQ_H
-#define CAST_IQ_H
+#ifndef GIP_CAST_IQ_H
+#define GIP_CAST_IQ_H
 
-#include <sstream>
 #include <stdexcept>
 #include <typeinfo>
 
@@ -40,63 +39,117 @@ namespace GeneralIterationPack {
   * class \c CastIQMember instead if it is appropriate.
   */
 template<class T>
-IterQuantityAccess<T>& cast_iq( AlgorithmState& state
-	, const std::string& iq_name );
+IterQuantityAccess<T>& cast_iq(
+	AlgorithmState& state, const std::string& iq_name );
 
 ///
 template<class T>
-const IterQuantityAccess<T>& cast_iq( const AlgorithmState& state
-	, const std::string& iq_name );
+const IterQuantityAccess<T>& cast_iq(
+	const AlgorithmState& state, const std::string& iq_name );
 
-}	// namespace GeneralIterationPack
+///
+/** Lookup an iteration quantity using its id and cast it
+ * to an <tt>IterQuantityAccess<T></tt> of the given type \c T.
+ *
+ * @param  state  [in] 
+ *
+ * If the iteration quantity with that id does not
+ * exist then a <tt>AlgorithmState::DoesNotExist</tt> exception
+ * will be thrown.  If the type of the iteration quantity
+ * is not of the type <tt>IterQuantityAcess<T></tt> (as determined
+ * by <tt>dynamic_cast<T></tt>) then the exception \c InvalidTypeCastException:
+ * will be thrown with a helpful error message.
+ *
+ * Note that using this function always cost just <tt>O(1)</tt> everytime
+ * it is called.
+ */
+template<class T>
+IterQuantityAccess<T>& cast_iq(
+	AlgorithmState& state, const AlgorithmState::iq_id_type iq_id, const std::string& iq_name );
+
+///
+template<class T>
+const IterQuantityAccess<T>& cast_iq(
+	const AlgorithmState& state, const AlgorithmState::iq_id_type iq_id, const std::string& iq_name );
+
+// Helper function
+
+void imp_cast_iq_throw_error(
+	const std::string&                 iq_name
+	,const std::string&                iq_is_type_name
+	,const std::string&                iq_want_type_name
+	);
+
+void imp_cast_iq_throw_error(
+	const AlgorithmState::iq_id_type   iq_id
+	,const std::string&                iq_name
+	,const std::string&                iq_is_type_name
+	,const std::string&                iq_want_type_name
+	);
 
 // ///////////////////
 // Inline definitions
 
-namespace {
-
 template<class T>
-inline
-void imp_cast_iq_throw_error(  const std::string& iq_name )
+//inline
+IterQuantityAccess<T>& cast_iq(
+	AlgorithmState& state, const std::string& iq_name )
 {
-	std::ostringstream omsg;
-	omsg
-		<< "cast_id<T>(state,iq_name) : Error, the iteration quantity \""
-		<< iq_name << "\" exists but it is not of the type IterQuantityAccess<"
-		<< typeid(T).name() << ">";
-	throw GeneralIterationPack::InvalidTypeCastException( omsg.str() );
-}
-
-}	// end namespace
-
-namespace GeneralIterationPack {
-
-template<class T>
-inline
-IterQuantityAccess<T>& cast_iq( AlgorithmState& state
-	, const std::string& iq_name )
-{
+	IterQuantity
+	 	&iq = state.iter_quant( iq_name );
 	IterQuantityAccess<T>
-		*p = dynamic_cast<IterQuantityAccess<T>*>( &state.iter_quant( iq_name ) );
+		*p = dynamic_cast<IterQuantityAccess<T>*>( &iq );
 			// will throw exception if iq_name does not exist
 	if( !p )
-		imp_cast_iq_throw_error<T>( iq_name );
+		imp_cast_iq_throw_error( iq_name, typeid(iq).name(), typeid(T).name() );
 	return *p;	
 }
 
 template<class T>
-inline
-const IterQuantityAccess<T>& cast_iq( const AlgorithmState& state
-	, const std::string& iq_name )
+//inline
+const IterQuantityAccess<T>& cast_iq(
+	const AlgorithmState& state, const std::string& iq_name )
 {
+	const IterQuantity
+	 	&iq = state.iter_quant( iq_name );
 	const IterQuantityAccess<T>
-		*p = dynamic_cast<const IterQuantityAccess<T>*>( &state.iter_quant( iq_name ) );
+		*p = dynamic_cast<const IterQuantityAccess<T>*>( &iq );
 			// will throw exception if iq_name does not exist
 	if( !p )
-		imp_cast_iq_throw_error<T>( iq_name );
+		imp_cast_iq_throw_error( iq_name, typeid(iq).name(), typeid(T).name() );
 	return *p;	
+}
+
+template<class T>
+//inline
+IterQuantityAccess<T>& cast_iq(
+	AlgorithmState& state, const AlgorithmState::iq_id_type iq_id, const std::string& iq_name )
+{
+	IterQuantity
+	 	&iq = state.iter_quant( iq_id );
+	IterQuantityAccess<T>
+		*p = dynamic_cast<IterQuantityAccess<T>*>( &iq );
+			// will throw exception if iq_name does not exist
+	if( !p )
+		imp_cast_iq_throw_error( iq_id, iq_name, typeid(iq).name(), typeid(T).name() );
+	return *p;	
+}
+
+template<class T>
+//inline
+const IterQuantityAccess<T>& cast_iq(
+	const AlgorithmState& state, const AlgorithmState::iq_id_type iq_id, const std::string& iq_name )
+{
+	IterQuantity
+	 	&iq = state.iter_quant( iq_id );
+	const IterQuantityAccess<T>
+		*p = dynamic_cast<const IterQuantityAccess<T>*>( &iq );
+			// will throw exception if iq_name does not exist
+	if( !p )
+		imp_cast_iq_throw_error( iq_id, iq_name, typeid(iq).name(), typeid(T).name() );
+	return *p;
 }
 
 }	// namespace GeneralIterationPack
 
-#endif	// CAST_IQ_H
+#endif	// GIP_CAST_IQ_H
