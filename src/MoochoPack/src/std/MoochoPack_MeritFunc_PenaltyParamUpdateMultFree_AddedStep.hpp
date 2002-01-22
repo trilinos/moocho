1571dc4953b2d5873ca20540b6abf344d3bf3e3d
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-22 17:18:28 +0000

    Don't need anymore.  All steps are now directly derived from AlgorithmStep

--- src/MoochoPack/src/std/MoochoPack_MeritFunc_PenaltyParamUpdateMultFree_AddedStep.hpp
+++ src/MoochoPack/src/std/MoochoPack_MeritFunc_PenaltyParamUpdateMultFree_AddedStep.hpp
@@ -31,23 +31,23 @@ public:
 
 	///
 	MeritFunc_PenaltyParamUpdateMultFree_AddedStep(
-		const merit_func_ptr_t& merit_func
-		, value_type small_mu = 1e-6
-		, value_type mult_factor = 1e-4
-		, value_type kkt_near_sol = 1.0
+		value_type   small_mu = 1e-6
+		,value_type  mult_factor = 1e-4
+		,value_type  kkt_near_sol = 1.0
 		);
 
 protected:
 
-	// /////////////////////////////////////////////////////////////
-	// Overridden from MeritFunc_PenaltyParamUpdateGuts_AddedStep
+	/** @name Overridden from MeritFunc_PenaltyParamUpdateGuts_AddedStep */
+	//@{
 
 	///
 	bool min_mu( rSQPState& s, value_type* min_mu ) const;
-
 	///
 	void print_min_mu_step( std::ostream& out
 		, const std::string& leading_str ) const;
+
+	//@}
 	
 };	// end class MeritFunc_PenaltyParamUpdateMultFree_AddedStep
 
