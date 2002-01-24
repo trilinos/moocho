35ccafd8e4080d1a407e5e3b967a236ee49a178b
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-24 23:00:14 +0000

    Added method initialize(), moved function implementations to a source file (as they should be) and changed the type for journal_out to ref_count_ptr<> for more flexible memory management

---
+++ src/IterationPack/src/IterationPack_AlgorithmTracker.cpp
@@ -0,0 +1,50 @@
+// ///////////////////////////////////////////////////////////////
+// AlgorithmTrack.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include "GeneralIterationPack/include/AlgorithmTrack.h"
+
+namespace GeneralIterationPack {
+
+AlgorithmTrack::AlgorithmTrack(const ostream_ptr_t& journal_out)
+	: journal_out_(journal_out)
+{}
+
+void AlgorithmTrack::initialize()
+{}
+	
+void AlgorithmTrack::output_iteration(const Algorithm& algo) const
+{}
+
+void AlgorithmTrack::output_final(const Algorithm& algo, EAlgoReturn algo_return) const
+{}
+
+void AlgorithmTrack::set_journal_out(const ostream_ptr_t& journal_out)
+{
+	journal_out_ = journal_out;
+}
+
+const AlgorithmTrack::ostream_ptr_t&
+AlgorithmTrack::get_journal_out() const
+{
+	return journal_out_;
+}
+
+std::ostream&
+AlgorithmTrack::journal_out() const
+{
+	return *journal_out_;
+}
+
+}	// end namespace GeneralIterationPack 
--- src/IterationPack/src/IterationPack_AlgorithmTracker.hpp
+++ src/IterationPack/src/IterationPack_AlgorithmTracker.hpp
@@ -19,6 +19,7 @@
 #include <iosfwd>
 
 #include "GeneralIterationPackTypes.h"
+#include "ref_count_ptr.h"
 
 namespace GeneralIterationPack {
 
@@ -33,34 +34,51 @@ namespace GeneralIterationPack {
 class AlgorithmTrack {
 public:
 
+	/** @name Public types */
+	//@{
+
+	///
+	typedef ReferenceCountingPack::ref_count_ptr<std::ostream>    ostream_ptr_t;
+
+	//@}
+
 	/** @name Constructors */
 	//@{
 
 	///
-	AlgorithmTrack(std::ostream& journal_out)
-		: journal_out_(&journal_out)
-			{}
-
+	/** Construct with an output stream for journal_out.
+	 *
+	 * Preconditions:<ul>
+	 * <li> <tt>journal_out.get() != NULL</tt> (throw <tt>std::invalid_argument</tt>).
+	 * </ul>
+	 */
+	AlgorithmTrack(const ostream_ptr_t& journal_out);
+	
 	//@}
-
+	
 	/** @name Algorithm iteration state notification */
 	//@{
+	
+	///
+	/** Reinitialize the track object right before it is used.
+	 *
+	 * The default implementation does nothing.
+	 */
+	virtual void initialize();
 
 	///
 	/** Output information about an iteration just completed.
 	  *
 	  * The default just does nothing.
 	  */
-	virtual void output_iteration(const Algorithm& algo) const
-			{}
+	virtual void output_iteration(const Algorithm& algo) const;
 
 	///
 	/** Output information about a just completed algorithm.
 	  *
 	  * The default just does nothing.
 	  */
-	virtual void output_final(const Algorithm& algo, EAlgoReturn algo_return) const
-			{}
+	virtual void output_final(const Algorithm& algo, EAlgoReturn algo_return) const;
 
 	//@}
 
@@ -68,23 +86,26 @@ public:
 	//@{
 
 	///
-	/** Set the journal file.
+	/** Set a smart pointer to the journal file.
 	  */
-	virtual void set_journal_out(std::ostream& journal_out)
-			{	journal_out_ = &journal_out; }
+	virtual void set_journal_out(const ostream_ptr_t& journal_out);
 
 	///
-	/** Return a reference to a std::ostream to be used to output debug information 
+	/** Get the smart pointer to the journal file.
+	 */
+	const ostream_ptr_t& get_journal_out() const;
+
+	///
+	/** Return a reference to a <tt>std::ostream</tt> to be used to output debug information 
 	  * and the like.
 	  */
-	virtual std::ostream& journal_out() const
-			{	return *const_cast<AlgorithmTrack*>(this)->journal_out_; }
+	virtual std::ostream& journal_out() const;
 
 	//@}
 
 private:
 
-	std::ostream *journal_out_;
+	ostream_ptr_t   journal_out_;
 
 	// not defined and not to be called
 	AlgorithmTrack();
