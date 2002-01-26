c74f3842530d0b075e1439edd09b1c47015f7f1c
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-26 17:49:26 +0000

    Added methods set_options() and get_options().  Updated documentation.

--- src/MoochoPack/src/MoochoPack_NLPAlgoConfig.hpp
+++ src/MoochoPack/src/MoochoPack_NLPAlgoConfig.hpp
@@ -18,6 +18,10 @@
 
 #include "ReducedSpaceSQPPackTypes.h"
 
+namespace OptionsFromStreamPack {
+	class OptionsFromStream;
+}
+
 namespace ReducedSpaceSQPPack {
 
 class rSQPAlgoContainer;
@@ -33,17 +37,58 @@ class rSQPAlgoInterface;
 class rSQPAlgo_Config {
 public:
 
-	/** @name exceptions */
+	/** @name Public types */
 	//@{
 
 	/// Thrown if NLP type is incompatible with this config
 	class InvalidNLPType : public std::logic_error
 	{public: InvalidNLPType(const std::string& what_arg) : std::logic_error(what_arg) {}};
+	///
+	typedef ReferenceCountingPack::ref_count_ptr<
+		const OptionsFromStreamPack::OptionsFromStream>             options_ptr_t;
 
 	//@}
 
-	// Virtual destructor
+	/** @name Constructors/destructors */
+	//@{
+
+	///
 	virtual ~rSQPAlgo_Config() {}
+
+	//@}
+
+	/** @name Set options */
+	//@{
+
+	///
+	/** Set the <tt>OptionsFromStreamPack::OptionsFromStream</tt> to extract the
+	 * options for the configuration from.
+	 *
+	 * @param  options  [in] Smart pointer to an <tt>OptionsFromStream</tt> object that
+	 *                  the solver will extract options from.  If <tt>options.get() != NULL</tt>
+	 *                  then this object must not be destroyed until <tt>this->set_options(other_options)</tt>
+	 *                  is called where <tt>other_options.get() != options.get()</tt> or \c this is
+	 *                  destoryed.  If <tt>options.get() == NULL</tt> then a default set of options will
+	 *                  be used.
+	 *
+	 * Postconditions:<ul>
+	 * <li> <tt>this->get_options().get() = options.get()</tt>
+	 * </ul>
+	 *
+	 * It is allowed for options in the underlying <tt>*options</tt> object to be modified
+	 * without recalling <tt>this->set_options()</tt> again.
+	 */
+	virtual void set_options( const options_ptr_t& options ) = 0;
+
+	///
+	/** Get the <tt>OptionsFromStream</tt> object being used to extract the options from.
+	 */
+	virtual const options_ptr_t& get_options() const = 0;
+
+	//@}
+
+	/** @name Algorithm configuration and initialization */
+	//@{
 
 	///
 	/** Configure the rSQP algorithm container with an rSQP algorithm object.
@@ -62,13 +107,17 @@ public:
 	 *
 	 * Preconditions:<ul>
 	 * <li> <tt>algo_cntr != NULL</tt> (throw <tt>???</tt>)
-	 * <li> <tt>algo_cntr->get_nlp() != NULL</tt> (throw <tt>???</tt>)
-	 * <li> <tt>algo_cntr->get_track() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_nlp().get() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_track().get() != NULL</tt> (throw <tt>???</tt>)
 	 * </ul>
 	 *
 	 * Postconditions:<ul>
-	 * <li> <tt>algo_cntr->algo() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_algo().get() != NULL</tt> (throw <tt>???</tt>)
 	 * </ul>
+	 *
+	 * Note that if the type of NLP return by <tt>algo_cntr->nlp()</tt> is not supported
+	 * by this configuration object, then a <tt>InvalidNLPType</tt> exception will be
+	 * thrown.
 	 */
 	virtual void config_algo_cntr(
 		rSQPAlgoContainer*       algo_cntr
@@ -86,9 +135,9 @@ public:
 	 *
 	 * Preconditions:<ul>
 	 * <li> <tt>algo_cntr != NULL</tt> (throw <tt>???</tt>)
-	 * <li> <tt>algo_cntr->get_nlp() != NULL</tt> (throw <tt>???</tt>)
-	 * <li> <tt>algo_cntr->get_track() != NULL</tt> (throw <tt>???</tt>)
-	 * <li> <tt>algo_cntr->get_algo() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_nlp().get() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_track().get() != NULL</tt> (throw <tt>???</tt>)
+	 * <li> <tt>algo_cntr->get_algo().get() != NULL</tt> (throw <tt>???</tt>)
 	 * </ul>
 	 * 
 	 * Postconditions:<ul>
@@ -98,6 +147,15 @@ public:
 	virtual void init_algo(
 		rSQPAlgoInterface* algo
 		) = 0;
+
+	//@}
+
+private:
+
+#ifdef DOXYGEN_COMPILE // Strictly for doxygen diagrams
+	///
+	OptionsFromStreamPack::OptionsFromStream    *options;
+#endif
 
 };	// end class rSQPAlgo_Config
 
