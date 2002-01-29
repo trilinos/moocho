bd85d67c746203c2d1c4e14933f85c5fbfd288bd
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-01-29 19:06:54 +0000

    Included the correct header file for LinAlgOpPack.h

--- src/AbstractLinAlgPack/src/abstract/interfaces/AbstractLinAlgPack_AbstractLinAlgOpPack.cpp
+++ src/AbstractLinAlgPack/src/abstract/interfaces/AbstractLinAlgPack_AbstractLinAlgOpPack.cpp
@@ -13,7 +13,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // above mentioned "Artistic License" for more details.
 
-#include "AbstractLinAlgPack/include/LinAlgOpPackDecl.h"
+#include "AbstractLinAlgPack/include/LinAlgOpPackDef.h"
 
 // Level 1 BLAS for Matrices
 
