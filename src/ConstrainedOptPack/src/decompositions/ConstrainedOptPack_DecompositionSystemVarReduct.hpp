7406079726d5399f02014be59801497892c64628
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-02-02 03:18:15 +0000

    Updating to be the implementation subclass for all variable reduction decompositons.  More work is needed.

--- src/ConstrainedOptPack/src/decompositions/ConstrainedOptPack_DecompositionSystemVarReduct.hpp
+++ src/ConstrainedOptPack/src/decompositions/ConstrainedOptPack_DecompositionSystemVarReduct.hpp
@@ -17,117 +17,258 @@
 #define DECOMPOSITION_SYSTEM_VAR_REDUCT_H
 
 #include "DecompositionSystem.h"
-#include "SparseSolverPack/include/BasisSystem.h"
+#include "AbstractLinAlgPack/include/BasisSystem.h"
+#include "AbstractLinAlgPack/include/VectorSpace.h"
+#include "StandardCompositionMacros.h"
+#include "StandardMemberCompositionMacros.h"
 
 namespace ConstrainedOptimizationPack {
 
 ///
-/** Specialization of DecompositionSystem for Variable Reduction basises {abstract}.
-  *
-  * This interface abstracts a variable reduction decomposition where:
-  *
-  \begin{verbatim}
+/** Specialization of \c DecompositionSystem for variable reduction decompositions.
+ *
+ * This interface abstracts a variable reduction decomposition where:
+ *
+ \verbatim
   
-  P*A'*Q = [C  N] 
-           [E  F]
+  Gc' = [ C  N ] 
+        [ E  F ]
 
-  Z = [ D ]
-      [ I ]
+  Z   = [ D ]
+        [ I ]
 
-      where:   D = -inv(C) * N
+  Uz  = F + E * D
 
-  A(indep)'*Y = C
+  Vz  = Gh(var_indep,:)' + Gh(var_dep,:)'*D
 
-  \end{verbatim}
-  *
-  * Here #C# is a r x r nonsingular matrix.
-  */
+      where:
+           C = Gc(var_dep,con_decomp)'     [nonsingular]
+           N = Gc(var_indep,con_decomp)'
+           E = Gc(var_dep,con_undecomp)'
+           F = Gc(var_indep,con_undecomp)'
+           D = -inv(C) * N
+ \endverbatim
+ *
+ * Above, \a C is a <tt>r x r</tt> nonsingular matrix.  Subclasses define
+ * how \c Y is defined which in turn determines how \c R, \c Uy and \c Vy are
+ * defined.
+ *
+ * The implementation of this subclass is completly determined by an aggregate
+ * <tt>BasisSytem</tt> object.  Since the <tt>BasisSystem</tt> interface does
+ * not allow any permutations of the basis to be performed ???.
+ *
+ * <b>Subclass implementors notes:</b>
+ *
+ * It is up to subclasses to override \c factory_R(), \c factory_Uy() and
+ * \c factory_Vy() in order to define the types for \c R, \c Uy and \c Vy
+ * respectively.
+ *
+ * ToDo: Finish documentation!
+ */
 class DecompositionSystemVarReduct : public DecompositionSystem {
 public:
 
-	/** @name types */
+	/** @name Public types */
 	//@{
 
 	///
-	typedef DecompositionSystem	inherited;
-
-	//@}
-
-	/** @name Exceptions */
-	//@{
-
-	/// Thrown if the basis given in set_basis(..) is structurally singular
-	typedef BasisSystem::StructuralSingularityException	StructuralSingularityException;
-
-	/// Thrown if the basis given in set_basi(...) is numerically singular
-	typedef BasisSystem::NumericalSingularityException	NumericalSingularityException;
-
-	/// Thrown if an invalid basis is passed to #this->set_basis()#
-	typedef BasisSystem::InvalidBasis					InvalidBasis;								
-
-	/// Thrown if #this->get_basis()# is called before #this->set_basis()#, or #this->update_decomp()#
-	typedef BasisSystem::NoBasisSelected				NoBasisSelected;								
-	
-	/// Thrown if #this->solve_C()#, or #this->N()# is called before #this->update_decomp()#
-	typedef inherited::NoDecompositionSelected			NoDecompositionSelected;
-
+	typedef DecompositionSystem	                                          inherited;
 	///
-	typedef inherited::InvalidMatrixType				InvalidMatrixType;
+	typedef BasisSystem::NumericallySingular                              NumericallySingular;
+	///
+	typedef ReferenceCountingPack::ref_count_ptr<const BasisSystem>       basis_sys_ptr_t;
+	///
+	enum EExplicitImplicit {
+		MAT_IMP_EXPLICIT
+		,MAT_IMP_IMPLICIT
+		,MAT_IMP_AUTO
+	};
 
 	//@}	
 
-	/// Return the current basis being used A.
-	virtual void get_basis(IVector* row_perm, IVector* col_perm
-		, size_type* rank) const = 0;
+	/** @name Constructors / initializers */
+	//@{
+
+	/// Set whether to use explicit or implicit <tt>D = -inv(C)*N</tt> matrix.
+	STANDARD_MEMBER_COMPOSITION_MEMBERS(EExplicitImplicit,D_imp)
+	/// Set whether to use explicit or implicit <tt>Uz = F + E * D</tt> matrix.
+	STANDARD_MEMBER_COMPOSITION_MEMBERS(EExplicitImplicit,Uz_imp)
+	/// Set whether to use explicit or implicit <tt>Vz = Gh(var_indep,:)' + Gh(var_dep,:)'*D</tt> matrix.
+	STANDARD_MEMBER_COMPOSITION_MEMBERS(EExplicitImplicit,Vz_imp)
 
 	///
-	/** Solves the systems #op(C) * x = b#.
-	  *
-	  * This operation solves the linear system:\\
-	  *
-	  * #C * x = b#,  for #trans == BLAS_Cpp::no_trans#\\
-	  * #C' * x = b#,  for #trans == BLAS_Cpp::trans#\\
-	  *
-	  * Preconditions (rank as returned from the last call to get_basis(....,&rank)):
-	  * \begin{itemize}
-	  * \item #b.size() == rank# (throw std::length_error)
-	  * \item #x.size() == rank# (throw std::length_error)
-	  * \end{itemize} 
-	  */
-	virtual void solve_C(const VectorSlice& b, BLAS_Cpp::Transp trans
-		, VectorSlice* x) const = 0;
+	/** Construct a variable reduction decomposition.
+	 *
+	 * Calls <tt>this->initialize()</tt>.
+	 *
+	 * Preconditions:<ul>
+	 * <li> ???
+	 * </ul>
+	 *
+	 * Postconditions:<ul>
+	 * <li> <tt>this->factory_Z().get() != NULL</tt>
+	 * </ul>
+	 */
+	DecompositionSystemVarReduct(
+		const VectorSpace::space_ptr_t     &space_x   = NULL
+		,const VectorSpace::space_ptr_t    &space_c   = NULL
+		,const VectorSpace::space_ptr_t    &space_h   = NULL
+		,const basis_sys_ptr_t             &basis_sys = NULL
+		,EExplicitImplicit                 D_imp      = MAT_IMP_AUTO
+		,EExplicitImplicit                 Uz_imp     = MAT_IMP_AUTO
+		,EExplicitImplicit                 Vz_imp     = MAT_IMP_AUTO
+		);
+
+	//@}
 
 	///
-	/** Solves the set of linear systems #op(C) * X = B#.
-	  *
-	  * This operation solves the set of linear systems:\\
-	  *
-	  * #C * X = B#,  for #trans == BLAS_Cpp::no_trans#\\
-	  * #C' * X = B#,  for #trans == BLAS_Cpp::trans#\\
-	  *
-	  * Preconditions (rank as returned from the last call to get_basis(....,&rank)):
-	  * \begin{itemize}
-	  * \item #B.rows() == rank# (throw std::length_error)
-	  * \item #X.rows() == rank# (throw std::length_error)
-	  * \item #X.cols() == B.cols()# (throw std::length_error)
-	  * \end{itemize} 
-	  */
-	virtual void solve_C(const GenMatrixSlice& B, BLAS_Cpp::Transp trans
-		, GenMatrixSlice* X) const = 0;
+	/** Initialize.
+	 *
+	 * @param  space_x
+	 *             [in] Vector space for variables \c x.
+	 * @param  space_c
+	 *             [in] Vector space for general equalities \c c.
+	 * @param  space_h
+	 *             [in] Vector space for general inequalities \c h.
+	 * @param  basis_sys
+	 *             [in] The <tt>BasisSystem</tt> object that defines the
+	 *             variable reduction that the decomposition is based on.
+	 *             This object must be fully initialized before this
+	 *             method is called.  The object <tt>*basis_sys</tt> must
+	 *             not be altered while \c this is still using it.  It is
+	 *             allowed for <tt>basis_sys.get() == NULL</tt> in which
+	 *             case \c this will not be fully initialized.
+	 *
+	 * Preconditions:<ul>
+	 * <li> ToDo: Spell these out!
+	 * </ul>
+	 *
+	 * Postconditions:<ul>
+	 * <li> ToDo: Spell these out!
+	 * </ul>
+	 */
+	void initialize(
+		const VectorSpace::space_ptr_t     &space_x
+		,const VectorSpace::space_ptr_t    &space_c
+		,const VectorSpace::space_ptr_t    &space_h
+		,const basis_sys_ptr_t             &basis_sys
+		);
 
-	/// Return a reference to the C matrix
-	virtual const MatrixWithOp& C() const = 0;
+	/** @name Access */
+	//@{
+	
+	///
+	const VectorSpace::space_ptr_t& space_x() const;
+	///
+	const VectorSpace::space_ptr_t& space_c() const;
+	///
+	const VectorSpace::space_ptr_t& space_h() const;
+	///
+	const basis_sys_ptr_t& basis_sys() const;
 
-	/// Return a reference to the N matrix
-	virtual const MatrixWithOp& N() const = 0;
+	//@}
 
-	/// Return a reference to the E matrix if it exists
-	virtual const MatrixWithOp& E() const = 0;
+	/** @name Overridden from DecompositionSystem */
+	//@{
 
-	/// Return a reference to the F matrix if it exists
-	virtual const MatrixWithOp& F() const = 0;
+	///
+	size_type n() const;
+	///
+	size_type m() const;
+	///
+	size_type r() const;
+	///
+	const mat_fcty_ptr_t factory_Z() const;
+	///
+	const mat_fcty_ptr_t factory_Uz() const;
+	///
+	const mat_fcty_ptr_t factory_Vz() const;
+	///
+	/**
+	 *
+	 * Preconditions:<ul>
+	 * <li> <tt>this->space_x().get() != NULL</tt> (throw <tt>std::logic_error</tt>)
+	 * <li> <tt>this->space_c().get() != NULL</tt> (throw <tt>std::logic_error</tt>)
+	 * <li> [<tt>Gh != NULL</tt>] <tt>this->space_h().get() != NULL</tt> (throw <tt>std::logic_error</tt>)
+	 * </ul>
+	 */
+	void update_decomp(
+		std::ostream              *out
+		,EOutputLevel             olevel
+		,ERunTests                test_what
+		,const MatrixWithOp       &Gc
+		,const MatrixWithOp       *Gh
+		,MatrixWithOp             *Z
+		,MatrixWithOp             *Y
+		,MatrixWithOpNonsingular  *R
+		,MatrixWithOp             *Uz
+		,MatrixWithOp             *Uy
+		,MatrixWithOp             *Vz
+		,MatrixWithOp             *Vy
+		) const;
+
+	//@}
+
+protected:
+
+	// ToDo: Add pure virtual method to uninitialize Y, R, Uy and Vy and return C if references!
+	// ToDo: Add pure virtual method to initialize Y, R, Uy and Vy given C and D!
+
+private:
+
+	// //////////////////////////////////
+	// Private data members
+	
+#ifdef DOXYGEN_COMPILE
+	AbstractLinAlgPack::BasisSystem       *basis_sys;
+	VectorSpace                           *space_x;
+	VectorSpace                           *space_c;
+	VectorSpace                           *space_h;
+#else
+	basis_sys_ptr_t                       basis_sys_;
+	VectorSpace::space_ptr_t              space_x_;
+	VectorSpace::space_ptr_t              space_c_;
+	VectorSpace::space_ptr_t              space_h_;
+#endif
+	// //////////////////////////////////
+	// Private member functions
+
+	// not defined and not to be called!
+	DecompositionSystemVarReduct(const DecompositionSystemVarReduct&);
+	DecompositionSystemVarReduct& operator=(const DecompositionSystemVarReduct&);
 
 };	// end class DecompositionSystemVarReduct
+
+// //////////////////////////////////////////
+// Inline members
+
+inline
+const VectorSpace::space_ptr_t&
+DecompositionSystemVarReduct::space_x() const
+{
+	return space_x_;
+}
+
+inline
+const VectorSpace::space_ptr_t&
+DecompositionSystemVarReduct::space_c() const
+{
+	return space_c_;
+}
+
+inline
+const VectorSpace::space_ptr_t&
+DecompositionSystemVarReduct::space_h() const
+{
+	return space_h_;
+}
+
+inline
+const DecompositionSystemVarReduct::basis_sys_ptr_t&
+DecompositionSystemVarReduct::basis_sys() const
+{
+	return basis_sys_;
+}
 
 }	// end namespace ConstrainedOptimizationPack
 
