7e239fcb925703b018b3b92bb19f01005c4d33f8
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-02-02 03:16:48 +0000

    Updating DecompositionSystem interface for general inequalites and explicit R matrix.  Also added methods for returning factory objects to create the decomposition matrix objects.  More changes may be needed before this interface is finished.

--- src/ConstrainedOptPack/src/decompositions/ConstrainedOptPack_DecompositionSystem.hpp
+++ src/ConstrainedOptPack/src/decompositions/ConstrainedOptPack_DecompositionSystem.hpp
@@ -42,6 +42,9 @@ namespace ConstrainedOptimizationPack {
  * <li> <tt>[Y , Z]</tt> is nonsingular
  * </ul>
  *
+ * The matrix factory objects returned by ??? are ment to have a lifetime that is
+ * independent of \c this.
+ *
  * ToDo: Finish documentation!
  */
 class DecompositionSystem {
@@ -51,8 +54,23 @@ public:
 	//@{
 
 	///
+	typedef ReferenceCountingPack::ref_count_ptr<
+		const AbstractFactoryPack::AbstractFactory<MatrixWithOpNonsingular> >    mat_nonsing_fcty_ptr_t;
+	///
+	typedef ReferenceCountingPack::ref_count_ptr<
+		const AbstractFactoryPack::AbstractFactory<MatrixWithOp> >               mat_fcty_ptr_t;
+	///
 	class InvalidMatrixType : public std::logic_error
 	{public: InvalidMatrixType(const std::string& what_arg) : std::logic_error(what_arg) {}};
+	/// Enumeration for the amount of output to create from <tt>update_decomp()</tt>.
+	enum EOutputLevel {
+		PRINT_NONE			= 0,
+		PRINT_BASIC_INFO	= 1,
+		PRINT_ITER_VECTORS	= 2,
+		PRINT_EVERY_THING	= 3
+		};
+	/// Enumeration for if to run internal tests or not.
+	enum ERunTests { RUN_TESTS, NO_TESTS };
 
 	//@}
 
@@ -100,6 +118,46 @@ public:
 	 * or <tt>Range1D::Invalid</tt> if <tt>this->r() == this->m()<tt>
 	 */
 	virtual Range1D con_undecomp() const;
+
+	//@}
+
+	/** @name Matrix factories */
+	//@{
+
+	///
+	/** Return a matrix factory object for <tt>Z</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Z() const = 0;
+
+	///
+	/** Return a matrix factory object for <tt>Y</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Y() const = 0;
+
+	///
+	/** Return a matrix factory object for <tt>R</tt>.
+	 */
+	virtual const mat_nonsing_fcty_ptr_t factory_R() const = 0;
+	
+	///
+	/** Return a matrix factory object for <tt>Uz</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Uz() const = 0;
+
+	///
+	/** Return a matrix factory object for <tt>Uy</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Uy() const = 0;
+
+	///
+	/** Return a matrix factory object for <tt>Vz</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Vz() const = 0;
+
+	///
+	/** Return a matrix factory object for <tt>Vy</tt>
+	 */
+	virtual const mat_fcty_ptr_t factory_Vy() const = 0;
 
 	//@}
 
@@ -164,31 +222,86 @@ public:
 	 * <li> [<tt>Vz != NULL</tt>] <tt>Vz.space_rows().is_compatible(Z.space_rows())</tt>
 	 * <li> [<tt>Vy != NULL</tt>] <tt>Vy.space_cols().is_compatible(*Gh->space_rows()) == true</tt>
 	 * <li> [<tt>Vy != NULL</tt>] <tt>Vy.space_rows().is_compatible(Y.space_rows())</tt>
+	 * <li> The behaviors of all of the participating matrices must not be altered by changes to the other matrices.
 	 * </ul>
 	 *
+	 * @param  out [out] If <tt>out!=NULL</tt> then output is printed to this stream
+	 *             depending on the value of \c olevel.
+	 * @param olevel
+	 *             [in] Determines the amount of output to print to \c *out.
+	 *             The exact type of output is determined by the implementing
+	 *             subclass but here is the sugguested behavior:<ul>
+	 *             <li> \c PRINT_NONE : Don't print anything (same as <tt>out==NULL</tt>).
+	 *             <li> \c PRINT_BASIC_INFO : Only print basic information about
+	 *                  how the decomposition is formed.
+	 *                  Amount of output = \c O(1).
+	 *             <li> \c PRINT_ITER_VECTORS : Prints out important vectors computed
+	 *                  durring the computations (usually only durring testing).
+	 *                  This level is only useful for debugging.
+	 *                  Amount of output = \c O(n).
+	 *             <li> \c PRINT_EVERY_THING : Print out nearly every important
+	 *                  quantity that is computed (except for the output matrices
+	 *                  themselves, clients can do that) while the matrices are being
+	 *                  formed or tests are being conducted.  This level is only
+	 *                  useful for debugging.
+	 *                  Amount of output = <tt>O(m*n)</tt>
+	 *             </ul>
+	 * @param test_what
+	 *             [in] Determines if internal validation tests are performed.
+	 *             The post conditions for the output matrices are not checked
+	 *             internally.  This is something that client can (and should)
+	 *             do independently (see \c DecompositionSystemTester).  Values:<ul>
+	 *             <li> \c RUN_TESTS : As many validation/consistency tests
+	 *                  are performed internally as possible.  If a test
+	 *                  fails then a \c TestFailed execption will be thrown.
+	 *                  The subclasses determine what the tests are and
+	 *                  what failing a test means.
+	 *             <li> \c NO_TEST : No tests are performed internally.  This is
+	 *                  to allow the fastest possible execution.
+	 *             </ul>
 	 * @param  Gc  [in] The matrix for which the range/null decomposition is defined.
 	 * @param  Gh  [in] An auxlillary matrix that will have the range/null decompositon applied to.
 	 *             It is allowed for <tt>Gh == NULL</tt>.
 	 * @param  Z   [out] On output represents the <tt>n x (n-r)</tt> null space	matrix such that
-	 *             <tt>Gc(:,con_decomp) * Z == 0</tt>.
+	 *             <tt>Gc(:,con_decomp) * Z == 0</tt>.  This matrix object must have been created
+	 *             by <tt>this->factory_Z()->create()</tt>.
 	 * @param  Y   [out] On output represents the <tt>n x r</tt> range space matrix	such that
-	 *             <tt>[ Y  Z ]</tt> is nonsingular.
+	 *             <tt>[ Y  Z ]</tt> is nonsingular.  This matrix object must have been created
+	 *             by <tt>this->factory_Y()->create()</tt>.
 	 * @param  R   [out] On output represents the nonsingular <tt>r x r</tt> matrix <tt>Gc(:,con_decomp) * Y</tt>.
+	 *             This matrix object must have been created by <tt>this->factory_R()->create()</tt>.
 	 * @param  Uz  [in/out] If <tt>Uz != NULL</tt> (<tt>this->m() > this->r()</tt> only) then on output
 	 *             <tt>*Uz</tt> represents the <tt>(m-r) x (n-r)</tt> matrix <tt>Gc(:,con_undecomp) * Z</tt>.
 	 *             If <tt>this->m() == this->r()</tt> then <tt>Uz == NULL</tt> must be true.
+	 *             If <tt>Uz!=NULL</tt>, then this matrix object must have been created by
+	 *             <tt>this->factory_Uz()->create()</tt>.
 	 * @param  Uy  [in/out] If <tt>Uy != NULL</tt> (<tt>this->m() > this->r()</tt> only) then on output
 	 *             <tt>*Uy</tt> represents the <tt>(m-r) x r</tt> matrix <tt>Gc(:,con_undecomp) * Y</tt>.
 	 *             If <tt>this->m() == this->r()</tt> then <tt>Uy == NULL</tt> must be true.
+	 *             If <tt>Uy!=NULL</tt>, then this matrix object must have been created by
+	 *             <tt>this->factory_Uy()->create()</tt>.
 	 * @param  Vz  [in/out] If <tt>Vz != NULL</tt> (<tt>Gh != NULL</tt> only) then on output
 	 *             <tt>*Vz</tt> represents the <tt>mI x (n-r)</tt> matrix <tt>Gh * Z</tt>.
 	 *             If <tt>Gh == NULL</tt> then <tt>Vz == NULL</tt> must be true.
+	 *             If <tt>Vz!=NULL</tt>, then this matrix object must have been created by
+	 *             <tt>this->factory_Vz()->create()</tt>.
 	 * @param  Vy  [in/out] If <tt>Vy != NULL</tt> (<tt>Gh != NULL</tt> only) then on output
 	 *             <tt>*Vy</tt> represents the <tt>mI x r</tt> matrix <tt>Gh * Y</tt>.
 	 *             If <tt>Gh == NULL</tt> then <tt>Vy == NULL</tt> must be true.
+	 *             If <tt>Vy!=NULL</tt>, then this matrix object must have been created by
+	 *             <tt>this->factory_Vy()->create()</tt>.
+	 *
+	 * Note that this method requires that all of the output matrix objects \c Z, \c Y, \c R,
+	 * \c Uz, \c Uy, \c Vz and \c Vy must be independent of \c this and of each other.
+	 * For example, the behavior of \c R must be be altered if \c this is destroyed or if
+	 * \c Z is modified of destroyed.  This requirment constrains the implementations somewhat
+	 * but makes things much easier for the client and gives the client much more power.
 	 */
 	virtual void update_decomp(
-		const MatrixWithOp        &Gc
+		std::ostream              *out
+		,EOutputLevel             olevel
+		,ERunTests                test_what
+		,const MatrixWithOp       &Gc
 		,const MatrixWithOp       *Gh
 		,MatrixWithOp             *Z
 		,MatrixWithOp             *Y
