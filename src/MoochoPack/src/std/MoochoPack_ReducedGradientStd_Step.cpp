21d7119a98feb10b7d671bf73a41656f01f6edea
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-02-05 18:54:02 +0000

    Updated for new design.

--- src/MoochoPack/src/std/MoochoPack_ReducedGradientStd_Step.cpp
+++ src/MoochoPack/src/std/MoochoPack_ReducedGradientStd_Step.cpp
@@ -18,27 +18,26 @@
 
 #include <ostream>
 
-#include "../../include/std/ReducedGradientStd_Step.h"
-#include "../../include/rsqp_algo_conversion.h"
+#include "ReducedSpaceSQPPack/include/std/ReducedGradientStd_Step.h"
+#include "ReducedSpaceSQPPack/include/rsqp_algo_conversion.h"
 #include "GeneralIterationPack/include/print_algorithm_step.h"
-#include "ConstrainedOptimizationPack/include/VectorWithNorms.h"
-#include "SparseLinAlgPack/include/MatrixWithOp.h"
-#include "LinAlgPack/include/LinAlgOpPack.h"
-#include "LinAlgPack/include/VectorClass.h"
-#include "LinAlgPack/include/VectorOut.h"
+#include "AbstractLinAlgPack/include/VectorWithOpMutable.h"
+#include "AbstractLinAlgPack/include/VectorWithOpOut.h"
+#include "AbstractLinAlgPack/include/MatrixWithOpNonsingular.h"
+#include "AbstractLinAlgPack/include/LinAlgOpPack.h"
 
-namespace LinAlgOpPack {
-	using SparseLinAlgPack::Vp_StMtV;
-}
+namespace ReducedSpaceSQPPack {
 
-bool ReducedSpaceSQPPack::ReducedGradientStd_Step::do_step(Algorithm& _algo
-	, poss_type step_poss, GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss)
+bool ReducedGradientStd_Step::do_step(
+	Algorithm& _algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+	, poss_type assoc_step_poss
+	)
 {
+	using BLAS_Cpp::trans;
 	using LinAlgOpPack::V_MtV;
-	using LinAlgPack::norm_inf;
 
-	rSQPAlgo	&algo	= rsqp_algo(_algo);
-	rSQPState	&s		= algo.rsqp_state();
+	rSQPAlgo    &algo   = rsqp_algo(_algo);
+	rSQPState   &s      = algo.rsqp_state();
 
 	EJournalOutputLevel olevel = algo.algo_cntr().journal_output_level();
 	std::ostream& out = algo.track().journal_out();
@@ -49,25 +48,35 @@ bool ReducedSpaceSQPPack::ReducedGradientStd_Step::do_step(Algorithm& _algo
 		print_algorithm_step( _algo, step_poss, type, assoc_step_poss, out );
 	}
 
+	// Get iteration quantities
+	IterQuantityAccess<VectorWithOpMutable>
+		&Gf_iq  = s.Gf(),
+		&rGf_iq = s.rGf();
+	IterQuantityAccess<MatrixWithOp>
+		&Z_iq = s.Z();
+
 	// rGf = Z' * Gf
-	V_MtV( &s.rGf().set_k(0).v(), s.Z().get_k(0), BLAS_Cpp::trans, s.Gf().get_k(0)() );
+	V_MtV( &rGf_iq.set_k(0), Z_iq.get_k(0), trans, Gf_iq.get_k(0) );
 
 	if( static_cast<int>(olevel) >= static_cast<int>(PRINT_ALGORITHM_STEPS) ) {
-		out	<< "\n||rGf||inf = "	<< s.rGf().get_k(0).norm_inf() << std::endl;
+		out	<< "\n||rGf||inf = "	<< rGf_iq.get_k(0).norm_inf() << std::endl;
 	}
 
 	if( static_cast<int>(olevel) >= static_cast<int>(PRINT_VECTORS) ) {
-		out << "\nrGf_k =\n" << s.rGf().get_k(0)();
+		out << "\nrGf_k =\n" << rGf_iq.get_k(0);
 	}
 
 	return true;
 }
 
-void ReducedSpaceSQPPack::ReducedGradientStd_Step::print_step( const Algorithm& algo
-	, poss_type step_poss, GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss
-	, std::ostream& out, const std::string& L ) const
+void ReducedGradientStd_Step::print_step(
+	const Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+	,poss_type assoc_step_poss, std::ostream& out, const std::string& L
+	) const
 {
 	out
 		<< L << "*** Evaluate the reduced gradient of the objective funciton\n"
 		<< L << "rGf_k = Z_k' * Gf_k\n";
 }
+
+} // end namespace ReducedSpaceSQPPack
--- src/MoochoPack/src/std/MoochoPack_ReducedGradientStd_Step.hpp
+++ src/MoochoPack/src/std/MoochoPack_ReducedGradientStd_Step.hpp
@@ -16,28 +16,28 @@
 #ifndef REDUCED_GRADIENT_STD_STEP_H
 #define REDUCED_GRADIENT_STD_STEP_H
 
-#include "../rSQPAlgo_StepBaseClasses.h"
-#include "../rSQPAlgo.h"
-#include "Misc/include/StandardCompositionRelationshipsPack.h"
+#include "ReducedSpaceSQPPack/include/ReducedSpaceSQPPackTypes.h"
+#include "GeneralIterationPack/include/AlgorithmStep.h"
 
 namespace ReducedSpaceSQPPack {
 
 ///
-/** rGf_k = Z_k' * Gf_k
+/** Computes the reducecd gradient of the objective <tt>rGf_k = Z_k' * Gf_k</tt>
   */
-class ReducedGradientStd_Step : public ReducedGradient_Step {
+class ReducedGradientStd_Step
+	: public GeneralIterationPack::AlgorithmStep // doxygen needs full path
+{
 public:
 
-	// ////////////////////
-	// Overridden
-
+	/** @name Overridden from AlgorithmStep */
+	//@{
 	///
 	bool do_step(Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
 		, poss_type assoc_step_poss);
-
 	///
 	void print_step( const Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
 		, poss_type assoc_step_poss, std::ostream& out, const std::string& leading_str ) const;
+	//@}
 
 };	// end class ReducedGradientStd_Step
 
