9441f4d8ae1a8e54f83489ee2cc59bdc4473534f
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-05-10 18:17:45 +0000

    Changed name to CheckDecompositionFromPy and updated to make more general.  This has been tested.

---
+++ src/MoochoPack/src/std/MoochoPack_CheckDecompositionFromPy_Step.cpp
@@ -0,0 +1,136 @@
+// ////////////////////////////////////////////////////////////////////////////
+// CheckDecompositionFromPy_Step.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include <typeinfo>
+#include <limits>
+
+#include "ReducedSpaceSQPPack/include/std/CheckDecompositionFromPy_Step.h"
+#include "ReducedSpaceSQPPack/include/rsqp_algo_conversion.h"
+#include "GeneralIterationPack/include/print_algorithm_step.h"
+#include "AbstractLinAlgPack/include/VectorWithOp.h"
+#include "AbstractLinAlgPack/include/LinAlgOpPack.h"
+
+namespace ReducedSpaceSQPPack {
+
+CheckDecompositionFromPy_Step::CheckDecompositionFromPy_Step(
+	const new_decomp_strategy_ptr_t   &new_decomp_strategy
+	,value_type                       max_decomposition_cond_change_frac
+	)
+	:new_decomp_strategy_(new_decomp_strategy)
+	,max_cond_( 0.01 / std::numeric_limits<value_type>::epsilon() )
+	,max_decomposition_cond_change_frac_( max_decomposition_cond_change_frac )
+{
+	reset();
+}
+
+void CheckDecompositionFromPy_Step::reset() {
+	beta_min_ = std::numeric_limits<value_type>::max();
+}
+
+// Overridden
+
+bool CheckDecompositionFromPy_Step::do_step(
+	Algorithm& _algo, poss_type step_poss, GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss
+	)
+{
+	rSQPAlgo                &algo       = rsqp_algo(_algo);
+	rSQPState               &s          = algo.rsqp_state();
+	const Range1D           equ_decomp  = s.equ_decomp();
+	EJournalOutputLevel     olevel      = algo.algo_cntr().journal_output_level();
+	std::ostream            &out        = algo.track().journal_out();
+
+	// print step header.
+	if( static_cast<int>(olevel) >= static_cast<int>(PRINT_ALGORITHM_STEPS) ) {
+		using GeneralIterationPack::print_algorithm_step;
+		print_algorithm_step( algo, step_poss, type, assoc_step_poss, out );
+	}
+
+	bool select_new_decomposition = false;
+
+	const value_type
+		small_num = std::numeric_limits<value_type>::min(),
+		beta = s.py().get_k(0).norm_inf() / (s.c().get_k(0).sub_view(equ_decomp)->norm_inf()+small_num);
+
+	if( (int)olevel >= (int)PRINT_ALGORITHM_STEPS ) {
+		out	<< "\nbeta = ||py||/||c|| = " << beta << std::endl;
+	}
+
+	if( beta + 1.0 < beta_min_ ) {
+		beta_min_ = beta + 1.0;
+	}
+	else {
+		if( (beta + 1.0)/ beta_min_ > max_decomposition_cond_change_frac() ) {
+			if( (int)olevel >= (int)PRINT_BASIC_ALGORITHM_INFO ) {
+				out	<< "Select a new decomposition"
+						<< " (k = " << algo.state().k() << ")\n"
+					<< "beta_change = ( ||py||/||c|| = " << beta
+						<< " ) / ( min ||py||/||c|| = " << beta_min_ << " )\n"
+					<< "beta_change = " << (beta/beta_min_) << " > max_decomposition_cond_change_frac = "
+						<< max_decomposition_cond_change_frac() << std::endl;
+			}
+			select_new_decomposition = true;
+		}
+	}
+	if( !select_new_decomposition && beta > max_cond() ) {
+		if( (int)olevel >= (int)PRINT_BASIC_ALGORITHM_INFO ) {
+			out	<< "\nConditioning of decomposition matrix is > " << beta
+				<< " > max_cond = " << max_cond() << std::endl
+				<< "Selecting a new decomposition ... "
+				<< " (k = " << algo.state().k() << ")\n";
+		}
+		select_new_decomposition = true;
+	}
+
+	if(select_new_decomposition) {
+		reset();
+		return new_decomp_strategy().new_decomposition(algo,step_poss,type,assoc_step_poss);
+	}
+
+	return true;		
+}
+
+void CheckDecompositionFromPy_Step::print_step(
+	const Algorithm& algo, poss_type step_poss
+	,GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss
+	,std::ostream& out, const std::string& L ) const
+{
+	out
+		<< L << "default: beta_min = inf\n"
+		<< L << "         max_decomposition_cond_change_frac = " << max_decomposition_cond_change_frac() << std::endl
+		<< L << "         max_cond = 0.01 * mach_eps\n"
+		<< L << "beta = norm_inf(py_k) / (norm_inf(c_k(equ_decomp))+small_number)\n"
+		<< L << "select_new_decomposition = false\n"
+		<< L << "if beta + 1 < beta_min then\n"
+		<< L << "  beta_min = beta + 1\n"
+		<< L << "else\n"
+		<< L << "  if (beta + 1) / beta_min > max_decomposition_cond_change_frac then\n"
+		<< L << "    select_new_decomposition = true\n"
+		<< L << "  end\n"
+		<< L << "end\n"
+		<< L << "if beta > max_cond then\n"
+		<< L << "  select_new_decomposition = true\n"
+		<< L << "end\n"
+		<< L << "if select_new_decomposition == true then\n"
+		<< L << "  new decomposition selection : " << typeid(new_decomp_strategy()).name() << std::endl
+		;
+	new_decomp_strategy().print_new_decomposition(
+		rsqp_algo(algo),step_poss,type,assoc_step_poss,out, L + "    " );
+	out
+		<< L << "  end new decomposition selection\n"
+		<< L << "end\n"
+		;
+}
+
+}	// end namespace ReducedSpaceSQPPack 
---
+++ src/MoochoPack/src/std/MoochoPack_CheckDecompositionFromPy_Step.hpp
@@ -0,0 +1,93 @@
+// ////////////////////////////////////////////////////////////////////////////
+// CheckDecompositionFromPy_Step.h
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#ifndef CHECK_DECOMPOSITION_FROM_PY_STEP_H
+#define CHECK_DECOMPOSITION_FROM_PY_STEP_H
+
+#include "NewDecompositionSelection_Strategy.h"
+#include "GeneralIterationPack/include/AlgorithmStep.h"
+#include "StandardCompositionMacros.h"
+#include "StandardMemberCompositionMacros.h"
+
+namespace ReducedSpaceSQPPack {
+
+///
+/** Check if the decomposition is going singular and if it is
+ * select a new decomposition.
+ *
+ * This steps checks if the decomposition is going singular by looking
+ * to see if the computation for the range space step looks bad.
+ *
+ * In particular we want to know how \c cond(R) is changing.  We know that:
+ \verbatim
+ py = -inv(R) * c
+ --> ||py|| <= ||inv(R)|| * ||c||
+ --> ||py|| / ||c|| <= ||inv(R)||
+ --> ( ||py|| / ||c|| ) * ||R|| <= ||inv(R)|| * ||R|| = cond(R)
+ \endverbatim
+ *
+ * If we assume <tt>||R|| > 1</tt> we know that <tt>cond(R) > beta = ||py||/||c||</tt>
+ * so if \c beta is very large then cond(R) is even larger.  Since the best decomposition
+ * we can find may be fairly illconditioned we may not want to use and absolute measure
+ * of \c beta to determine if the decomposition is illconditioned.  Instead we will look
+ * at the change in \c beta between iterations and if \c beta gets very much
+ * larger than its minimum value (i.e. <tt>beta / beta_min > max_cond_change</tt> 
+ * ( default = 10000 ) ) then we will select a new decomposition.  Also if
+ * <tt>beta > max_cond</tt> ( default = <tt>0.01 / mach_eps</tt> ) then we know we will be
+ * computing inacurate solutions so we must select a new decomposition.
+ */
+class CheckDecompositionFromPy_Step
+	: public GeneralIterationPack::AlgorithmStep // doxygen needs full name
+{
+public:
+
+	///
+	STANDARD_COMPOSITION_MEMBERS( NewDecompositionSelection_Strategy, new_decomp_strategy )
+	///
+	STANDARD_MEMBER_COMPOSITION_MEMBERS( value_type, max_decomposition_cond_change_frac )
+	///
+	STANDARD_MEMBER_COMPOSITION_MEMBERS( value_type, max_cond )
+
+	///
+	CheckDecompositionFromPy_Step(
+		const new_decomp_strategy_ptr_t   &new_decomp_strategy
+		,value_type                       max_decomposition_cond_change_frac = 100.0
+		);
+
+	/// Call the reset initialization of all defaults.
+	void reset();
+
+	/** @name Overridden from AlgorithmStep */
+	//@{
+	///
+	bool do_step(Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+		, poss_type assoc_step_poss);
+	///
+	void print_step( const Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+		, poss_type assoc_step_poss, std::ostream& out, const std::string& leading_str ) const;
+	//@}
+
+private:
+
+	value_type  beta_min_;
+
+	// Not defined and not to be called
+	CheckDecompositionFromPy_Step();
+
+}; // end class CheckDecompositionFromPy_Step
+
+} // end namespace ReducedSpaceSQPPack 
+
+#endif // CHECK_DECOMPOSITION_FROM_PY_STEP_H
