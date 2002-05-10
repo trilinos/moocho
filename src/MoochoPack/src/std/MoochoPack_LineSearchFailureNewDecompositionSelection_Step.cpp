51e8303702343145f6844a42bfcf18641b853c85
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-05-10 19:51:45 +0000

    Changed name to ..NewDecomposition... and updated.

---
+++ src/MoochoPack/src/std/MoochoPack_LineSearchFailureNewDecompositionSelection_Step.cpp
@@ -0,0 +1,102 @@
+// ////////////////////////////////////////////////////////////////////////////
+// LineSearchFailureNewDecompositionSelection_Step.cpp
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#include <ostream>
+#include <typeinfo>
+
+#include "ReducedSpaceSQPPack/include/std/LineSearchFailureNewDecompositionSelection_Step.h"
+#include "ReducedSpaceSQPPack/include/std/rSQPAlgorithmStepNames.h"
+#include "ReducedSpaceSQPPack/include/rsqp_algo_conversion.h"
+#include "ReducedSpaceSQPPack/include/ReducedSpaceSQPPackExceptions.h"
+#include "GeneralIterationPack/include/print_algorithm_step.h"
+
+namespace ReducedSpaceSQPPack {
+
+LineSearchFailureNewDecompositionSelection_Step::LineSearchFailureNewDecompositionSelection_Step(
+	const line_search_step_ptr_t        &line_search_step
+	,const new_decomp_strategy_ptr_t    &new_decomp_strategy
+	)
+	:line_search_step_(line_search_step)
+	,new_decomp_strategy_(new_decomp_strategy)
+	,last_ls_failure_k_(-100) // has not failed yet
+{}
+
+bool LineSearchFailureNewDecompositionSelection_Step::do_step(
+	Algorithm& _algo, poss_type step_poss, GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss
+	)
+{
+	try {
+		return line_search_step().do_step(_algo,step_poss,type,assoc_step_poss);
+	}
+	catch(const LineSearchFailure& lsf_excpt) {
+		rSQPAlgo	&algo	= rsqp_algo(_algo);
+		rSQPState	&s		= algo.rsqp_state();
+		NLP			&nlp	= algo.nlp();
+
+		EJournalOutputLevel olevel = algo.algo_cntr().journal_output_level();
+		std::ostream& out = algo.track().journal_out();
+
+		if( static_cast<int>(olevel) >= static_cast<int>(PRINT_BASIC_ALGORITHM_INFO) ) {
+			out	<< "\nLine search failed "
+				<< " (k = " << algo.state().k() << ")\n"
+				<< "LineSearchFailure description: " << lsf_excpt.what() << "\n";
+		}
+
+		if( last_ls_failure_k_ == s.k() - 1 ) {
+			if( static_cast<int>(olevel) >= static_cast<int>(PRINT_BASIC_ALGORITHM_INFO) ) {
+				out	<< "\nThe line search failed again even with a new decomposition!"
+					<< " (k = " << algo.state().k() << ")\n"
+					<< "We quit!\n";
+			}
+			throw;
+		}
+
+		if( static_cast<int>(olevel) >= static_cast<int>(PRINT_BASIC_ALGORITHM_INFO) ) {
+			out	<< "\nSelecting a new decomposition..."
+				<< " (k = " << algo.state().k() << ")\n";
+		}
+
+		last_ls_failure_k_ = s.k();
+		return new_decomp_strategy().new_decomposition(algo,step_poss,type,assoc_step_poss);
+	}
+	return false;	// will never be executed.
+}
+
+void LineSearchFailureNewDecompositionSelection_Step::print_step(
+	const Algorithm& algo
+	,poss_type step_poss, GeneralIterationPack::EDoStepType type, poss_type assoc_step_poss
+	,std::ostream& out, const std::string& L
+	) const
+{
+	out
+		<< L << "do line search step : " << typeid(line_search_step()).name() << std::endl;
+	line_search_step().print_step(algo,step_poss,type,assoc_step_poss,out,L+"    ");
+	out
+		<< L << "end line search step\n"
+		<< L << "if thrown line_search_failure then\n"
+		<< L << "  if line search failed at the last iteration also then\n"
+		<< L << "    throw line_search_failure\n"
+		<< L << "  end\n"
+		<< L << "  new decomposition selection : " << typeid(new_decomp_strategy()).name() << std::endl
+		;
+	new_decomp_strategy().print_new_decomposition(
+		rsqp_algo(algo),step_poss,type,assoc_step_poss,out, L + "    " );
+	out
+		<< L << "  end new decomposition selection\n"
+		<< L << "end\n"
+		;
+}
+
+} // end namespace ReducedSpaceSQPPack
---
+++ src/MoochoPack/src/std/MoochoPack_LineSearchFailureNewDecompositionSelection_Step.hpp
@@ -0,0 +1,75 @@
+// ////////////////////////////////////////////////////////////////////////////
+// LineSearchFailureNewDecompositionSelection_Step.h
+//
+// Copyright (C) 2001 Roscoe Ainsworth Bartlett
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the "Artistic License" (see the web site
+//   http://www.opensource.org/licenses/artistic-license.html).
+// This license is spelled out in the file COPYING.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// above mentioned "Artistic License" for more details.
+
+#ifndef LINE_SEARCH_FAILURE_NEW_DECOMPOSITION_SELECTION_H
+#define LINE_SEARCH_FAILURE_NEW_DECOMPOSITION_SELECTION_H
+
+#include "NewDecompositionSelection_Strategy.h"
+#include "GeneralIterationPack/include/AlgorithmStep.h"
+#include "StandardCompositionMacros.h"
+
+namespace ReducedSpaceSQPPack {
+
+///
+/** Directs the selection of a new decomposition if the line search fails.
+  *
+  * If the delegated line search Step object throws a \c LineSearchFailure
+  * exception, then this object directs the selection of a new
+  * decomposition.  If the very next iteration also results in a linesearch
+  * failure then we must quit.
+  */
+class LineSearchFailureNewDecompositionSelection_Step
+	: public GeneralIterationPack::AlgorithmStep // doxygen needs full path
+{
+public:
+
+	/// <<std comp>> members for LineSearch object.
+	STANDARD_COMPOSITION_MEMBERS( GeneralIterationPack::AlgorithmStep, line_search_step )
+
+	/// <<std comp>> members for Decomposition Select Strategy object.
+	STANDARD_COMPOSITION_MEMBERS( NewDecompositionSelection_Strategy, new_decomp_strategy )
+
+	///
+	LineSearchFailureNewDecompositionSelection_Step(
+		const line_search_step_ptr_t        &line_search_step
+		,const new_decomp_strategy_ptr_t    &new_decomp_strategy
+		);
+
+	/** @name Overridden from AlgorithmStep */
+	//@{
+	///
+	bool do_step(Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+		, poss_type assoc_step_poss);
+	///
+	void print_step( const Algorithm& algo, poss_type step_poss, GeneralIterationPack::EDoStepType type
+		, poss_type assoc_step_poss, std::ostream& out, const std::string& leading_str ) const;
+	//@}
+
+private:
+
+	int last_ls_failure_k_;
+
+	// not defined and not to be called
+	LineSearchFailureNewDecompositionSelection_Step();
+	LineSearchFailureNewDecompositionSelection_Step(
+		const LineSearchFailureNewDecompositionSelection_Step&);
+	LineSearchFailureNewDecompositionSelection_Step& operator=(
+		const LineSearchFailureNewDecompositionSelection_Step&);
+
+};	// end class LineSearchFailureNewDecompositionSelection_Step
+
+}	// end namespace ReducedSpaceSQPPack 
+
+#endif	// LINE_SEARCH_FAILURE_NEW_DECOMPOSITION_SELECTION_H
