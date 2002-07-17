168228bc24046578bcec8d15861a973c042b3be0
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-07-17 18:29:15 +0000

    Fixed type in question-mark operator.

--- src/AbstractLinAlgPack/src/serial/interfaces/AbstractLinAlgPack_LinAlgOpPackHack.cpp
+++ src/AbstractLinAlgPack/src/serial/interfaces/AbstractLinAlgPack_LinAlgOpPackHack.cpp
@@ -147,7 +147,9 @@ void LinAlgOpPack::Vp_StPtMtV(
 	VectorSpace::vec_mut_ptr_t
 		ay =  ( ay_space.get()
 				? ay_space->create_member()
-				: mmp::rcp(new VectorWithOpMutableDense(BLAS_Cpp::rows(P.rows(),P.cols(),P_trans))) ),
+				: mmp::rcp_implicit_cast<VectorWithOpMutable>(
+					mmp::rcp(new VectorWithOpMutableDense(BLAS_Cpp::rows(P.rows(),P.cols(),P_trans)))
+					) ),
 		ax = ( M_trans == no_trans ? M.space_rows() : M.space_cols() ).create_member();
 	(VectorDenseMutableEncap(*ay))() = *y;
 	(VectorDenseMutableEncap(*ax))() = x;
