8c4f676910875fa08e22df4c322f942692a534c5
Roscoe A. Bartlett <rabartl@sandia.gov>
2002-07-18 14:19:22 +0000

    Added math.h header.

--- src/ConstrainedOptPack/src/qpsolvers/ConstrainedOptPack_QPSolverRelaxedTester.cpp
+++ src/ConstrainedOptPack/src/qpsolvers/ConstrainedOptPack_QPSolverRelaxedTester.cpp
@@ -13,6 +13,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // above mentioned "Artistic License" for more details.
 
+#include <math.h>
+
 #include "ConstrainedOptimizationPack/include/QPSolverRelaxedTester.h"
 #include "AbstractLinAlgPack/include/MatrixSymWithOp.h"
 #include "AbstractLinAlgPack/include/VectorSpace.h"
